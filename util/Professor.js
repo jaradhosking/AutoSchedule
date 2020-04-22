916173545c103406a22f5dbddc5ef28cd5a2cfe7
Manny <mjonson34@ymail.com>
2020-04-22 01:40:17 -0400

    Python to JS

--- util/Professor.js
+++ util/Professor.js
@@ -1,25 +1,16 @@
-class Professor:
-    def __init__(self, name, avgGPA, avgCourseGPA, rmp_r, rmp_ss):
-        self.name = name #string, name of professor
-        self.avgGPA = avgGPA #float, average GPA of professor per Course Critique
-        self.avgCourseGPA = avgCourseGPA #dictionary, keys are courses, values are professor's avg GPA for that course
-        self.rmp_r = rmp_r #float, average rate my professors rating of professor
-        self.rmp_ss = rmp_ss #int, number of rate my professors responses
+class Professor {
+    constructor (self, name, avgGPA, avgCourseGPA, rmp_r, rmp_ss):
+        this.name = name // string, name of professor
+        this.avgGPA = avgGPA // float, average GPA of professor per Course Critique
+        this.avgCourseGPA = avgCourseGPA // dictionary, keys are courses, values are professor's avg GPA for that course
+        this.rmp_r = rmp_r // float, average rate my professors rating of professor
+        this.rmp_ss = rmp_ss // int, number of rate my professors responses
 
+    equals (o) {
+        return (o instanceof Professor) && (this.name == o.name);
+    }
 
-
-
-    def __eq__(self,obj):
-        return isinstance(obj,Professor) and self.name == obj.name
-
-
-
-
-    def __str__(self):
-        return self.name
-
-
-
-
-    def __repr__(self):
-        return self.__str__()
+    toString () {
+        return this.name;
+    }
+}
