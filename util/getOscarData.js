8045bce6ce8163c9407c5307a4ed53176093ec26
Manny <mjonson34@ymail.com>
2020-04-22 02:46:40 -0400

    Push often

--- util/getOscarData.js
+++ util/getOscarData.js
@@ -1,6 +1,9 @@
 
 function getRawHTML (sem, dept, cNum) {
     URL = `https://oscar.gatech.edu/pls/bprod/bwckctlg.p_disp_listcrse?term_in=${sem}&subj_in=${dept}&crse_in=${+xcNum}&schd_in=%`
-
-
+    $.get(URL, function (html) {
+        $(html).find("table").each(function () {
+            console.log(this);
+        })
+    })
 }
