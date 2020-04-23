817dfa79833306ab7d6e9d4f7fc890a3fd7e78fd
jarad <jhosking3@gatech.edu>
2020-04-23 02:52:15 -0400

    Signed-off-by: jarad <jhosking3@gatech.edu>

---
+++ util/buildSchedules2.js
@@ -0,0 +1,34 @@
+// courseCritiqueCourses
+// courseCritiqueProfessors
+// rmp
+// oscar
+
+// class Course {
+//     constructor(subject, numID, creditHours, avgGPA) {
+
+// class Section {
+//     constructor (CRN, course, professor, campus, building, timeSlots) {
+
+// class Professor {
+//     constructor (name, avgGPA, avgCourseGPA, rmp_r, rmp_ss) {
+
+// class Agenda {
+//     constructor(dow, start, end) {
+
+function buildSchedule(rcl, ocl, chr) {
+    let minCreds = chr[0]
+    let maxCreds = chr[1]
+
+    for (let i of rcl) {
+        
+    }
+    for (let j of ocl) {
+
+    }
+
+}
+
+
+
+//////////////////JARAD GO DOWN HERE
+function create{} BLAH BLA HBLAH
