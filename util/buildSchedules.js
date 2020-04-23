497037403ccd2449a3dccde23404d767f6ea5560
Manny <mjonson34@ymail.com>
2020-04-23 04:00:58 -0400

    Reformatted professor data

--- util/buildSchedules.js
+++ util/buildSchedules.js
@@ -11,6 +11,13 @@
 
 // class Professor {
 //     constructor (name, avgGPA, avgCourseGPA, rmp_r, rmp_ss) {
+// {
+//     name : name
+//     avgGPA : avgGPA
+//     avgCourseGPA : {course1GPA, ...}
+//     rmp_r : rating
+//     rmp_ss : numRatings
+// }
 
 // class Agenda {
 //     constructor(dow, start, end) {
@@ -20,6 +27,20 @@ function buildSchedule(rcl, ocl, chr) {
     let maxCreds = chr[1]
     let rCourses = []
     let oCourses = []
+    let rProfs = new Set()
+    let oProfs = new Set()
+
+    for (let i of rCourses) {
+        numID = /[0-9]+/g.exec(i)[0]
+        dept = /[A-Z]+/g.exec(i)[0]
+        rProfs.add(oscar[dept][numID]["Instructors"][0])
+    }
+
+    for (let i of oCourses) {
+        numID = /[0-9]+/g.exec(i)[0]
+        dept = /[A-Z]+/g.exec(i)[0]
+        oProfs.add(oscar[dept][numID]["Instructors"][0])
+    }
 
     for (let i of rcl) {
         numID = /[0-9]+/g.exec(i)[0]
@@ -47,4 +68,9 @@ function buildSchedule(rcl, ocl, chr) {
         chr = parseFloat(oscar["fall"][dept][numID][0]["credits"])
         oCourses.push(new Course(dept, numID, chr, avgGPA))
     }
+    for (let i of rcl) {
+        numID = /[0-9]+/g.exec(i)[0]
+        dept = /[A-Z]+/g.exec(i)[0]
+    }
+
 }
